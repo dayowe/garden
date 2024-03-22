a07a1151cc2ff201ccba447a33917f781dea72f8
Dayowe <dayowe@tutamail.com>
2024-03-22 11:32:38 +0100

    Support up to 8 sensors

--- soil_sensor_THC-S_RS485/soil_sensor_THC-S_RS485.ino
+++ soil_sensor_THC-S_RS485/soil_sensor_THC-S_RS485.ino
@@ -1,102 +1,106 @@
-#include <Wire.h>  // Include Wire library for I2C
-#include <Adafruit_GFX.h> // Include Adafruit graphics library
-#include <Adafruit_SSD1306.h> // Include Adafruit SSD1306 library
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 #include <PubSubClient.h>
 #include <ModbusMaster.h>
-#include <WiFi.h>  // Include the WiFi library
+#include <WiFi.h>
 #include "WiFiCredentials.h"
 #include "mqttCredentials.h"
 
-// MQTT Topics
-const char* temperatureTopic = "sensor/soil/thcs1/temperature";
-const char* moistureTopic = "sensor/soil/thcs1/moisture";
-const char* conductivityTopic = "sensor/soil/thcs1/conductivity";
+// MQTT Topics base
+const char* baseTopic = "soil/";
 
 // Instantiate ModbusMaster object
 ModbusMaster sensor;
 
-// Use Serial2 for RS485 communication on ESP32
 #define RS485Serial Serial2
-// RS485 Transmit Enable Pin - Not used since converter does automatic switching
-// Adjust pins according to your wiring if needed
-#define RXD2 16
-#define TXD2 17
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
 
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
-
-// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire);
-
-void preTransmission() {
-  // Not needed for automatic RS485 control
-}
-
-void postTransmission() {
-  // Not needed for automatic RS485 control
-}
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
+const int maxSensors = 8;
+int displaySensorID = 1;
+bool activeSensors[maxSensors] = {false};
+
+unsigned long previousMillis = 0;
+const long interval = 2000;
+
+void preTransmission() {}
+void postTransmission() {}
+
 void setup() {
-  // Connect to WiFi
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.println("Connecting to WiFi..");
   }
   Serial.println("Connected to WiFi");
-
-  // Set MQTT server
   client.setServer(mqttServer, mqttPort);
 
-  // Begin Serial Communication with Computer for debugging
   Serial.begin(115200);
-
-  // Begin Serial2 Communication for RS485 on pins 16 & 17 (RX: 16, TX: 17) adjust if needed
   RS485Serial.begin(4800, SERIAL_8N1, 16, 17);
-  
-  // Modbus communication setup
-  sensor.begin(1, RS485Serial); // Slave ID: 1
+
   sensor.preTransmission(preTransmission);
   sensor.postTransmission(postTransmission);
 
-  Serial.println("Reading Soil Sensor Data:");
-
-  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x64
+  // Initialize OLED display
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
     Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
+    for(;;);
   }
-  
-  // Clear the buffer.
-  display.clearDisplay();
+  display.display(); // Show initial screen (blank or splash screen)
+  delay(2000); // Short delay before clearing display
 
-  // add here any additional display setup you want to do initially
-  
-  // Display static text (just as an example, can be removed if not needed)
-  display.setTextSize(1);    // Normal 1:1 pixel scale
-  display.setTextColor(SSD1306_WHITE); // Draw white text
-  display.setCursor(0,0);   // Start at top-left corner
-  display.println(F("Soil Sensor Data:"));
-  
-  // Show the display buffer on the screen
+  // Detect Sensors
+  detectSensors();
+}
+
+void detectSensors() {
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0,0);
+  display.println(F("Detecting Sensors..."));
   display.display();
+
+  int detectedSensors = 0;
+
+  for (int sensorID = 1; sensorID <= maxSensors; sensorID++) {
+    sensor.begin(sensorID, RS485Serial);
+    uint8_t result = sensor.readHoldingRegisters(0x0000, 1);
+    if (result == sensor.ku8MBSuccess) {
+      activeSensors[sensorID - 1] = true;
+      Serial.print("Sensor detected: ID ");
+      Serial.println(sensorID);
+      detectedSensors++; // Increment the count for each detected sensor
+    }
+    // without short delay only first sensor is detected
+    delay(20);
+  }
+
+  // Move the completion message here, outside the loop, using detectedSensors variable
+  display.clearDisplay();  // Clearing the display again for showing completion status
+  display.setCursor(0,0);
+  display.print("Detection complete. Detected ");
+  display.print(detectedSensors); // Using detectedSensors variable here
+  display.println(" sensors.");
+  display.display(); 
+  delay(2000);
 }
 
 void reconnect() {
-  // Loop until we're reconnected
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
-    // Attempt to connect
     if (client.connect("ESP32Client", mqttUser, mqttPassword)) {
       Serial.println("connected");
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
       Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
       delay(5000);
     }
   }
@@ -107,81 +111,93 @@ void loop() {
     reconnect();
   }
   client.loop();
-  uint8_t result;
-  float humidity, temperature, conductivity;
-  
-  // Read 3 registers starting at 0x0000
-  result = sensor.readHoldingRegisters(0x0000, 3);
-  
-  if (result == sensor.ku8MBSuccess) {
-    humidity = sensor.getResponseBuffer(0) / 10.0; // sensor returns dat aas 0.1%RH, so divide by 10
-    temperature = sensor.getResponseBuffer(1) / 10.0; // sensor returns data as 0.1°C, so divide by 10
-    conductivity = sensor.getResponseBuffer(2) / 1000.0; // Ensure division by a floating-point number for accurate calculation
 
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    
+    // Only read and publish if the sensor is active
+    if (activeSensors[displaySensorID - 1]) {
+      readAndPublishSensorData(displaySensorID);
+
+      // Moving to next sensor
+      do {
+        displaySensorID++;
+        if (displaySensorID > maxSensors) {
+          displaySensorID = 1;
+        }
+      } while (!activeSensors[displaySensorID - 1]);
+    }
+  }
+}
+
+void readAndPublishSensorData(int sensorID) {
+  sensor.begin(sensorID, RS485Serial);
+  uint8_t result = sensor.readHoldingRegisters(0x0000, 3);
+
+  // Initialize display for new data presentation
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0,0);
+
+  // Always display the Sensor ID
+  display.print("Sensor ID: ");
+  display.println(sensorID);
+
+  display.println(F(""));
+
+  if (result == sensor.ku8MBSuccess) {
+    float humidity = sensor.getResponseBuffer(0) / 10.0;
+    float temperature = sensor.getResponseBuffer(1) / 10.0;
+    float conductivity = sensor.getResponseBuffer(2) / 1000.0;
+
+    // Log to serial for debug purposes
     Serial.print("Humidity: ");
     Serial.print(humidity);
     Serial.println("% RH");
-
     Serial.print("Temperature: ");
     Serial.print(temperature);
     Serial.println("°C");
-
     Serial.print("Conductivity: ");
     Serial.print(conductivity);
-    Serial.println("us/cm");
-  } else {
-    Serial.println("Failed to read from sensor, check wiring");
-  }
-  
-  delay(1000); // Poll every second
+    Serial.println(" mS/cm");
 
-  if (result == sensor.ku8MBSuccess) {
-    // Clear previous content
-    display.clearDisplay();
-    
-    // Set cursor to column 0, row 0
-    display.setCursor(0, 0);
-    display.setTextSize(1); // Normal 1:1 pixel scale
-    display.setTextColor(SSD1306_WHITE); // Draw white text
-    display.println(F("Soil Sensor Data:"));
+    // Display sensor values on the OLED
+    display.print("Hum: ");
+    display.print(humidity);
+    display.println("% RH");
     display.println(F(""));
-    display.print(F("Temp:     "));
+    display.print("Temp: ");
     display.print(temperature);
     display.println(" C");
-
     display.println(F(""));
-
-    display.print(F("Moisture: "));
-    display.print(humidity);
-    display.println(" %");
-
-    display.println(F(""));
-    
-    display.print(F("EC:       "));
+    display.print("Cond: ");
     display.print(conductivity);
     display.println(" mS/cm");
-    
-    // Refresh the OLED display with new data
-    display.display();
-  } else {
-    // Handle failed read, maybe clear screen and show error?
-  }
-  // Convert float to string for publishing
-  char messageBuffer[20];
 
-  if (result == sensor.ku8MBSuccess) {
-    // Publish temperature
+    // Construct MQTT topics and publish sensor data
+    char topic[100];
+    char messageBuffer[20];
+    snprintf(topic, sizeof(topic), "%sthcs%d/temperature", baseTopic, sensorID);
     dtostrf(temperature, 1, 2, messageBuffer);
-    client.publish(temperatureTopic, messageBuffer);
-
-    // Publish moisture
+    client.publish(topic, messageBuffer);
+    
+    snprintf(topic, sizeof(topic), "%sthcs%d/moisture", baseTopic, sensorID);
     dtostrf(humidity, 1, 2, messageBuffer);
-    client.publish(moistureTopic, messageBuffer);
+    client.publish(topic, messageBuffer);
 
-    // Publish conductivity
-    dtostrf(conductivity, 6, 3, messageBuffer); // Adjusted precision to 3 decimal places for mS/cm.
-    client.publish(conductivityTopic, messageBuffer);
+    snprintf(topic, sizeof(topic), "%sthcs%d/conductivity", baseTopic, sensorID);
+    dtostrf(conductivity, 6, 3, messageBuffer);
+    client.publish(topic, messageBuffer);
+  } else {
+    Serial.print("Failed to read from sensor ");
+    Serial.println(sensorID);
+    // Optionally, you can also inform on the OLED screen about the failure
+    display.println("Read failed!");
   }
 
-  delay(2000); // Consider longer delay for readability
+  // Final step to actually show the data
+  display.display();
 }
